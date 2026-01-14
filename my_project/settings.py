f9fdeae9cf3f4f3576bb205f53b0bb22ea950614
Alex-L5 <almenkiti@aol.com>
2026-01-14 10:14:45 +0000

    Use pip to install a compatible version 6.5 of a package called whitenoise, recreate the requirements.txt file

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u*$f5thedoj-w&g_@rgsj#1uu#g^onwjubk5)96(ewlg6fl=+7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['my-django-basics-blog-app-d30042d67850.herokuapp.com'] 
 
